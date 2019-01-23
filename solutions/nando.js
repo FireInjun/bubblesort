7ed9ea089bc11ca1c7e0b0e24d5f607022cf7870
FireInjun <mk@fireinjun.com>
2019-01-23 17:32:31 -0500

    Added Nando's solution
    Optimized for loop

---
+++ solutions/nando.js
@@ -0,0 +1,13 @@
+// Nando solution
+function nandoBubbleSort(arr) {
+  for (let i = 0, l = arr.length; i < l; i++) {
+    for (let j = 1; j < l; j++) {
+      if (arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
