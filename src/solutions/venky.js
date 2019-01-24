fd9a8968bb1eb5b68c0b8d35a96df6be4b504678
FireInjun <mk@fireinjun.com>
2019-01-23 22:07:36 -0500

    Resetting line endings to LF

--- src/solutions/venky.js
+++ src/solutions/venky.js
@@ -4,7 +4,7 @@ function venkyBubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j < arr.length; j++) {
       if (arr[j - 1] > arr[j]) {
-        let temp = arr[j - 1];
+        const temp = arr[j - 1];
         arr[j - 1] = arr[j];
         arr[j] = temp;
       }
