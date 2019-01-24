341082ce0d62d49664bc75259c144b2117b0c585
FireInjun <mk@fireinjun.com>
2019-01-23 19:42:34 -0500

    Reformatted all JS files in src with prettier

--- src/solutions/venky.js
+++ src/solutions/venky.js
@@ -1,14 +1,14 @@
 // venky.js
 // Venky solution
 function venkyBubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arr.length; j++) {
-      if (arr[j - 1] > arr[j]) {
-        let temp = arr[j - 1];
-        arr[j - 1] = arr[j];
-        arr[j] = temp;
-      }
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr.length; j++) {
+            if (arr[j - 1] > arr[j]) {
+                let temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
     }
-  }
-  return arr;
+    return arr;
 }
--- src/testdata/small.js
+++ src/testdata/small.js
@@ -1,6 +1,5 @@
 // small.js
 
-
 // Small arrays for test usage
 // 10 indexes each
 const Set1 = [170, 141, 113, 125, 20, 67, 87, 153, 54, 199];
