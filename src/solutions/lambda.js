9b40bbe2bb84d45243e887a3fe104fb810906c4a
FireInjun <mk@fireinjun.com>
2019-01-23 21:53:33 -0500

    Resetting line endings to LF

--- src/solutions/lambda.js
+++ src/solutions/lambda.js
@@ -1,17 +1,17 @@
 // lambda.js
 // Lambda School model solution
 function lambdaBubbleSort(arr) {
-    let swappedValue;
-    do {
-        swappedValue = false;
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swappedValue = true;
-            }
-        }
-    } while (swappedValue);
-    return arr;
+  let swappedValue;
+  do {
+    swappedValue = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swappedValue = true;
+      }
+    }
+  } while (swappedValue);
+  return arr;
 }
--- src/solutions/nando.js
+++ src/solutions/nando.js
@@ -1,14 +1,14 @@
 // nando.js
 // Nando solution
 function nandoBubbleSort(arr) {
-    for (let i = 0, l = arr.length; i < l; i++) {
-        for (let j = 1; j < l; j++) {
-            if (arr[j - 1] > arr[j]) {
-                let temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-            }
-        }
+  for (let i = 0, l = arr.length; i < l; i++) {
+    for (let j = 1; j < l; j++) {
+      if (arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
     }
-    return arr;
+  }
+  return arr;
 }
